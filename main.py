66cbb7e1e04576cc915dac2230bf92811b648724
RobsenGelana <robsengelana21@gmail.com>
2022-09-27 15:28:07 +0300

    creating a window for the app

---
+++ main.py
@@ -0,0 +1,26 @@
+from cgitb import text
+from curses import window
+from email.mime import image
+from tkinter import *
+from turtle import width
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manger")
+
+canvas = Canvas()
+lock_img = PhotoImage(file='logo.png')
+canvas.create_image(100, 102, image=lock_img)
+canvas.grid(padx=20, pady=20)
+
+
+
+
+
+
+window.mainloop()
