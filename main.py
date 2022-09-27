fde1d0d1bd62b16db7f5188afd499d85e041950e
RobsenGelana <robsengelana21@gmail.com>
2022-09-27 16:42:12 +0300

    creating a function that save the data

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    with open('data.txt', 'a') as data:
+        web_data = web_entry.get()
+        email = email_entry.get()
+        password = passwrd_entry.get()
+        data.write(f"{web_data} | {email} | {password} \n")
+    web_entry.delete(0, END)
+    passwrd_entry.delete(0, END)
+    web_entry.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -20,25 +29,27 @@ website = Label(text='Website')
 website.grid(column=0, row=1)
 
 web_entry = Entry(width=35)
+web_entry.focus()
 web_entry.grid(column=1, row=1)
 
 user_name = Label(text='Email/Username: ')
 user_name.grid(column=0, row=2)
 
 email_entry = Entry(width=35)
+email_entry.insert(0, 'example@.com')
 email_entry.grid(column=1,row=2)
 
 password_word = Label(text="Password")
 password_word.grid(column=0, row=3)
 
-passwrd_entry = Entry(width=21)
+passwrd_entry = Entry(width=35)
 passwrd_entry.grid(column=1, row=3)
 
 generate_password = Button(text='Generate Password')
 generate_password.grid(column=2, row=3)
 
-add = Button(text="Add", width=36)
-add.grid(column=1, row=4)
+add = Button(text="Add", width=36, command=save)
+add.grid(column=1, row=4,)
 
 
 
