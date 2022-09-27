be5519b4de0984678989c337f878ef342380514e
RobsenGelana <robsengelana21@gmail.com>
2022-09-27 19:23:52 +0300

    finishing the project

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
+from cgitb import text
 from tkinter import *
 from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-password_list = []
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
+    password_list = []
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
 
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
 
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
 
-random.shuffle(password_list)
+    random.shuffle(password_list)
 
-password = ""
-for char in password_list:
-  password += char
+    password = ""
+    for char in password_list:
+        password += char
 
-print(f"Your password is: {password}")
+    passwrd_entry.insert(0, password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     web_data = web_entry.get()
@@ -76,7 +78,7 @@ password_word.grid(column=0, row=3)
 passwrd_entry = Entry(width=35)
 passwrd_entry.grid(column=1, row=3)
 
-generate_password = Button(text='Generate Password')
+generate_password = Button(text='Generate Password', command=generate_password)
 generate_password.grid(column=2, row=3)
 
 add = Button(text="Add", width=36, command=save)
