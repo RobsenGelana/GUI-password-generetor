0675b60faa71a39e89cb901aad687253c53180c4
RobsenGelana <robsengelana21@gmail.com>
2022-09-27 15:31:40 +0300

    adding padding to screen

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ from turtle import width
 
 window = Tk()
 window.title("Password Manger")
+window.config(padx=20, pady=20)
 
 canvas = Canvas(height=200, width=200)
 lock_img = PhotoImage(file='logo.png')
-canvas.create_image(100, 102, image=lock_img)
+canvas.create_image(100, 100, image=lock_img)
 canvas.grid(padx=20, pady=20)
 
 
